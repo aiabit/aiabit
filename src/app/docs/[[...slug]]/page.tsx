b3b656aef889fd1a2f14274e33510bb9185bb7c5
Zack <lorenx@163.com>
2023-11-21 00:03:42 +0800

    feat: add docs page

---
+++ src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { allDocs } from "contentlayer/generated";
+import dayjs from "dayjs";
+import { getMDXComponent } from "next-contentlayer/hooks";
+
+import Anchor from "./Anchor";
+
+export const generateStaticParams = (): { slug: string[] }[] =>
+  allDocs.map((doc) => ({ slug: doc._raw.flattenedPath.split("/") }));
+
+export const generateMetadata = ({
+  params,
+}: {
+  params: { slug?: string[] };
+}): Metadata => {
+  const slug = params.slug?.join("/") ?? "";
+  const doc = allDocs.find((d) => d._raw.flattenedPath === slug);
+  if (!doc) {
+    return { title: "Not found" };
+  }
+  return { title: doc.title };
+};
+
+const PostLayout = ({ params }: { params: { slug?: string[] } }) => {
+  const slug = params.slug?.join("/") ?? "";
+  const doc = allDocs.find((d) => d._raw.flattenedPath === slug)!;
+  if (!doc) {
+    return <div>Not found</div>;
+  }
+
+  const Content = getMDXComponent(doc.body.code);
+
+  return (
+    <article className="mx-auto max-w-xl py-8">
+      <Anchor />
+      <div className="mb-8 text-center">
+        <time dateTime={doc.date} className="mb-1 text-xs text-gray-600">
+          {dayjs(doc.date).format("YYYY-MM-DD")}
+        </time>
+        <h1>{doc.title}</h1>
+      </div>
+      <Content />
+    </article>
+  );
+};
+
+export default PostLayout;
