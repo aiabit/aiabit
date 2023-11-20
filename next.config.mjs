fd86bea81f26a46f64d04fb6e8a7c639bcc3a52f
Zack <lorenx@163.com>
2023-11-20 23:55:20 +0800

    feat: add docs
    feat: update content

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,20 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import { withContentlayer } from "next-contentlayer";
 await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  reactStrictMode: true,
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ["@aiabit/api", "@aiabit/auth", "@aiabit/db"],
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  webpack(config) {
+    config.infrastructureLogging = { level: "error" };
+    return config;
+  },
+};
 
-export default config;
+export default withContentlayer(config);
